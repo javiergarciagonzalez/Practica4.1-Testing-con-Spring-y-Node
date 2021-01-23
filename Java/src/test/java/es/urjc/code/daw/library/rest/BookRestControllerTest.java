ba554afc57d1ddc266e5e0be08e8daa50080d138
Dionisio Cortés Fernández <cortesfernandezdionisio@gmail.com>
2021-01-23 17:08:01 +0100

    Add tests with MockMvc

---
+++ Java/src/test/java/es/urjc/code/daw/library/rest/BookRestControllerTest.java
@@ -0,0 +1,88 @@
+package es.urjc.code.daw.library.rest;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import es.urjc.code.daw.library.book.Book;
+import es.urjc.code.daw.library.book.BookService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class BookRestControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private BookService bookService;
+
+    @Test
+    public void getBooksTest() throws Exception {
+
+
+        List<Book> books = Arrays.asList(
+                new Book("SUEÑOS DE ACERO Y NEON",
+                        "Los personajes que protagonizan este relato sobreviven en una sociedad en decadencia a la que, no obstante, lograrán devolver la posibilidad de un futuro. Año 2484. En un mundo dominado por las grandes corporaciones, solo un hombre, Jordi Thompson, detective privado deslenguado y vividor, pero de gran talento y sentido d..."),
+                new Book("LA VIDA SECRETA DE LA MENTE",
+                        "La vida secreta de la mentees un viaje especular que recorre el cerebro y el pensamiento: se trata de descubrir nuestra mente para entendernos hasta en los más pequeños rincones que componen lo que somos, cómo forjamos las ideas en los primeros días de vida, cómo damos forma a las decisiones que nos constituyen, cómo soñamos y cómo imaginamos, por qué sentimos ciertas emociones hacia los demás, cómo los demás influyen en nosotros, y cómo el cerebro se transforma y, con él, lo que somos."),
+                new Book("CASI SIN QUERER",
+                        "El amor algunas veces es tan complicado como impredecible. Pero al final lo que más valoramos son los detalles más simples, los más bonitos, los que llegan sin avisar. Y a la hora de escribir sobre sentimientos, no hay nada más limpio que hacerlo desde el corazón. Y eso hace Defreds en este libro."),
+                new Book("TERMINAMOS Y OTROS POEMAS SIN TERMINAR",
+                        "Recopilación de nuevos poemas, textos en prosa y pensamientos del autor. Un sabio dijo una vez: «Pocas cosas hipnotizan tanto en este mundo como una llama y como la luna, será porque no podemos cogerlas o porque nos iluminan en la penumbra». Realmente no sé si alguien dijo esta cita o me la acabo de inventar pero deberían de haberla escrito porque el poder hipnótico que ejercen esa mujer de rojo y esa dama blanca sobre el ser humano es digna de estudio."),
+                new Book("LA LEGIÓN PERDIDA",
+                        "En el año 53 a. C. el cónsul Craso cruzó el Éufrates para conquistar Oriente, pero su ejército fue destrozado en Carrhae. Una legión entera cayó prisionera de los partos. Nadie sabe a ciencia cierta qué pasó con aquella legión perdida.150 años después, Trajano está a punto de volver a cruzar el Éufrates. ..."));
+
+
+        when(bookService.findAll()).thenReturn(books);
+
+        mvc.perform(get("/api/books/")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(5)))
+                .andExpect(jsonPath("$[0].title", equalTo("SUEÑOS DE ACERO Y NEON")));
+    }
+
+    @Test
+    @WithMockUser(username = "user", password = "pass", roles = "USER")
+    public void addBookTest() throws Exception {
+
+        Book book = new Book("How to outfox a billionaire", "https://blog.reedsy.com/book-title-generator/");
+        when(bookService.save(Mockito.any(Book.class))).thenReturn(book);
+
+        mvc.perform(post("/api/books/")
+                .content(new ObjectMapper().writeValueAsString(book))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.title", equalTo("How to outfox a billionaire")))
+                .andExpect(jsonPath("$.description", equalTo("https://blog.reedsy.com/book-title-generator/")));
+    }
+
+
+    @Test
+    @WithMockUser(username = "admin", password = "pass", roles = {"USER", "ADMIN"})
+    public void deleteBookTest() throws Exception {
+
+        mvc.perform(delete("/api/books/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+}
